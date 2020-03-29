2b7647599a690039987d9053cf0efc1a7bc3155e
Florencia <florenciaalmada7@hotmail.com>
2020-03-28 22:18:49 -0300

    Updates

---
+++ precache-manifest.54bbf8ff231686c4d21d454d0b9065a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aadc3397d047a8feddb48fc4f26a9280",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "01612f810ce87b38b3c3",
+    "url": "/robofriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "b5142e9c740260199031",
+    "url": "/robofriends/static/css/main.9be3713e.chunk.css"
+  },
+  {
+    "revision": "01612f810ce87b38b3c3",
+    "url": "/robofriends/static/js/2.aef0a387.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.aef0a387.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5142e9c740260199031",
+    "url": "/robofriends/static/js/main.16817ec6.chunk.js"
+  },
+  {
+    "revision": "46690a7b9ddc777b3b4c",
+    "url": "/robofriends/static/js/runtime-main.ba4515c0.js"
+  }
+]);
